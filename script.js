856c2063e114a389e7f21330401a81fce48ee905
Kerem Üzer <keremuzer33@gmail.com>
2023-04-13 12:41:21 +0300

    add hover effect

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
-    container.appendChild(document.createElement("div"));
+    let newGrid = document.createElement("div");
+    newGrid.classList = "grid";
+    container.appendChild(newGrid);
   }
 }
