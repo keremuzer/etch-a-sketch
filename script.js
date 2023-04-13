b7b2446980fb95ff60d6e7d178a0ab7599fb1819
Kerem Üzer <keremuzer33@gmail.com>
2023-04-13 13:33:04 +0300

    add size control

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const container = document.querySelector(".container");
+let containerSize = 16;
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let newGrid = document.createElement("div");
-    newGrid.classList = "grid";
-    container.appendChild(newGrid);
+createContainer(containerSize);
+
+function createContainer(containerSize){
+  let grids = document.querySelectorAll(".grid");
+  grids.forEach(grid => grid.remove());
+  for (let i = 0; i < containerSize; i++) {
+    for (let j = 0; j < containerSize; j++) {
+      let newGrid = document.createElement("div");
+      newGrid.classList.add("grid");
+      container.appendChild(newGrid);
+    }
   }
 }
+
+small = document.getElementById("small").addEventListener("click", function(){
+  containerSize = 16;
+  createContainer(containerSize);
+  let grids = document.querySelectorAll(".grid");
+  grids.forEach(grid => {
+    grid.style.width = "39px";
+    grid.style.height = "39px";
+  });
+});
+
+medium = document.getElementById("medium").addEventListener("click", function(){
+  containerSize = 32;
+  createContainer(containerSize);
+  let grids = document.querySelectorAll(".grid");
+  grids.forEach(grid => {
+    grid.style.width = "19px";
+    grid.style.height = "19px";
+  });
+});
+
+big = document.getElementById("big").addEventListener("click", function(){
+  createContainer(64);
+  let grids = document.querySelectorAll(".grid");
+  grids.forEach(grid => {
+    grid.style.width = "9px";
+    grid.style.height = "9px";
+  });
+});
