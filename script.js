08482a46f122428ea2fc6190e3e87bf687df8014
Kerem Üzer <keremuzer33@gmail.com>
2023-04-13 12:22:59 +0300

    add grid

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    container.appendChild(document.createElement("div"));
+  }
+}
